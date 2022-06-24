3f720c3b3cbba1e32156b5301e0514914581b6c2
GiraffeSummer <gidoselten@hotmail.com>
2022-06-24 17:01:10 +0200

    guildcreate and delete, hikageuser badge added

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -8,24 +8,8 @@ export default (client: Client): void => {
             return;
         }
 
-        //register badges as choices to select command
-        //await registerBadges();
-
         await client.application.commands.set(Commands);
 
         console.log(`${client.user.username} is online`);
     });
 };
-
-async function registerBadges() {
-    //get all badges
-    const badges = await prisma.badge.findMany();
-
-    const badgeCommand = Commands.find(b => b.name == 'badge')
-    const badgeOption = badgeCommand.options.find(b => b.name == 'select')
-    const badgeChoices = badges.map(b => {
-        return { name: b.name, value: b.name }
-    });
-    //@ts-ignore //yes I know, but this is required
-    badgeOption.options[0].choices = badgeChoices
-}
