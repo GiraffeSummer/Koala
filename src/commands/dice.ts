f5e6e26648970842db28a56e8e3dcd9f1fdb832e
GiraffeSummer <gidoselten@hotmail.com>
2023-08-14 15:08:00 +0200

    added dice rolling

---
+++ src/commands/dice.ts
@@ -0,0 +1,32 @@
+import { CommandInteraction, Client, ApplicationCommandType, ApplicationCommandOptionType } from "discord.js";
+import { Command } from "../Command";
+import { RandomNum } from '../lib/Functions'
+import { DiceRoll } from '@dice-roller/rpg-dice-roller';
+
+const roll = new DiceRoll('4d6');
+
+//just copy and paste this commands, it has a few things pre made so it's easy as template
+export default {
+    name: "dice",
+    description: "Roll a dice",
+    type: ApplicationCommandType.ChatInput,
+    options: [{
+        type: ApplicationCommandOptionType.String,
+        name: 'notation',
+        description: 'How many sides should the dice have?',
+        required: true
+    },
+    ],
+    run: async (client: Client, interaction: CommandInteraction) => {
+        const notation = interaction.options.get('notation').value as string 
+        const roll = new DiceRoll(notation);
+        await interaction.followUp({
+            embeds: [{
+                title: `Result: ${roll}:game_die:`,
+                color: 0x4169e1,
+                author: { name: interaction.user.username, icon_url: interaction.user.avatarURL() },
+                // description: `${(roll.multi) ? `\`${out.result.join(', ')}\`` : ""}`
+            }]
+        });
+    }
+} as Command;
