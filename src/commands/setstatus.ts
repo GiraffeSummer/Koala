fe306fd08e6506b7304fa94b16b2d216c8512f7a
GiraffeSummer <gidoselten@hotmail.com>
2023-09-17 17:50:21 +0200

    added profanity filtering

--- src/commands/setstatus.ts
+++ src/commands/setstatus.ts
@@ -1,6 +1,7 @@
-import { CommandInteraction, Client ,ApplicationCommandType,ApplicationCommandOptionType} from "discord.js";
+import { CommandInteraction, Client, ApplicationCommandType, ApplicationCommandOptionType } from "discord.js";
 import { Command } from "../Command";
 import prisma, { where } from "../lib/db";
+import * as profanity from '../lib/ProfanityFilter'
 
 //just copy and paste this commands, it has a few things pre made so it's easy as template
 export default {
@@ -17,19 +18,28 @@ export default {
     run: async (client: Client, interaction: CommandInteraction) => {
         const status = interaction.options.get('status')?.value as string || null
 
-        const line = (await prisma.user.upsert({
-            where: { uid: interaction.user.id },
-            update: { status },
-            create: {
-                status,
-                uid: interaction.user.id,
-                name: interaction.user.username
-            },
-        }));
+        const hasCurseWords = await profanity.check(status)
 
-        await interaction.followUp({
-            ephemeral: true,
-            content: 'Status was updated'
-        });
+        if (!hasCurseWords) {
+            const line = (await prisma.user.upsert({
+                where: { uid: interaction.user.id },
+                update: { status },
+                create: {
+                    status,
+                    uid: interaction.user.id,
+                    name: interaction.user.username
+                },
+            }));
+
+            await interaction.followUp({
+                ephemeral: true,
+                content: 'Status was updated'
+            });
+        } else {
+            await interaction.followUp({
+                ephemeral: true,
+                content: 'You have illegal words in your status'
+            });
+        }
     }
 } as Command;
