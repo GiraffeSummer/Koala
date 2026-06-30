abf5ec3948c62409f61c65121f8e81174e3deca5
GiraffeSummer <gidoselten@hotmail.com>
2026-06-30 20:29:42 +0200

    better ollama function to warmup the model

--- src/lib/ollama.ts
+++ src/lib/ollama.ts
@@ -10,27 +10,48 @@ function hasPrefetchExpired(): boolean {
 
 
 export default async function (prompt: string, model: string = 'llama3.2'): Promise<{ success: true, response: string } | { success: false, msg: string }> {
-    if (!prefetchSuccess || hasPrefetchExpired()) {
-        console.log('prefetching')
-        const prefetch = await fetch(`${process.env.OLLAMA_URL}`);//TODO: do a precheck once for online
-        prefetchTime = new Date()
-        if (prefetch.status == 200) {
-            prefetchSuccess = true;
-        } else {
-            return { success: false, msg: 'Service is unavailable right now' }
+    try {
+
+        if (!prefetchSuccess || hasPrefetchExpired()) {
+            console.log('prefetching')
+            const prefetch = await fetch(`${process.env.OLLAMA_URL}/api/generate`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    model,
+                    keep_alive: '10m',
+                    stream: false
+                })
+            })
+            prefetchTime = new Date()
+            if (prefetch.ok) {
+                prefetchSuccess = true;
+            } else {
+                return { success: false, msg: 'Service is unavailable right now' }
+            }
         }
+
+        const res = await fetch(`${process.env.OLLAMA_URL}/api/generate`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                model: model ?? 'llama3.2',
+                prompt: prompt,
+                stream: false
+            })
+        });
+
+        const data: { response?: string } = await res.json();
+
+
+        if (!data.response) {
+            return { success: false, msg: 'No response received from model' };
+        }
+
+        return { success: true, response: data.response }
+    } catch (error) {
+        prefetchSuccess = false;
+        prefetchTime = undefined;
+        return { success: false, msg: 'Somethign went wrong' }
     }
-
-    const res = await fetch(`${process.env.OLLAMA_URL}/api/generate`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            model: model ?? 'llama3.2',
-            prompt: prompt,
-            stream: false
-        })
-    });
-
-    const data: { response?: string } = await res.json();
-    return { success: true, response: data.response }
 }
