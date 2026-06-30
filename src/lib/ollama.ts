d72060c686c5821662e5e392c49b06ddca5e040f
GiraffeSummer <gidoselten@hotmail.com>
2026-06-30 20:19:04 +0200

    better ollama responses

--- src/lib/ollama.ts
+++ src/lib/ollama.ts
@@ -1,17 +1,36 @@
-export default async function (prompt: string, model: string = 'llama3.2:3b') {
+let prefetchTime: Date | undefined = undefined
+let prefetchSuccess = false;
 
-    // const prefetch = await fetch(`${process.env.OLLAMA_URL}`);//TODO: do a precheck once for online
+const timeAmount = 30 * 60 * 1000
+function hasPrefetchExpired(): boolean {
+    if (prefetchTime == undefined) return true
+    const elapsedTime = new Date().getTime() - prefetchTime.getTime()
+    return elapsedTime >= timeAmount;
+}
+
+
+export default async function (prompt: string, model: string = 'llama3.2'): Promise<{ success: true, response: string } | { success: false, msg: string }> {
+    if (!prefetchSuccess || hasPrefetchExpired()) {
+        console.log('prefetching')
+        const prefetch = await fetch(`${process.env.OLLAMA_URL}`);//TODO: do a precheck once for online
+        prefetchTime = new Date()
+        if (prefetch.status == 200) {
+            prefetchSuccess = true;
+        } else {
+            return { success: false, msg: 'Service is unavailable right now' }
+        }
+    }
 
     const res = await fetch(`${process.env.OLLAMA_URL}/api/generate`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            model: model ?? 'llama3.2:3b',
+            model: model ?? 'llama3.2',
             prompt: prompt,
             stream: false
         })
     });
 
     const data: { response?: string } = await res.json();
-    return data.response
+    return { success: true, response: data.response }
 }
